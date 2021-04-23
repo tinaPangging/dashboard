2ab98dbf367c9b2baba1ab175c8f8da1e6a708ad
tinaPangging <tina.pangging90@gmail.com>
2021-04-22 23:45:21 -0300

    final clean up

--- src/landingPage/LandingPage.jsx
+++ src/landingPage/LandingPage.jsx
@@ -1,13 +1,15 @@
-import Grid from "@material-ui/core/Grid";
+
+import Grid from '@material-ui/core/Grid';
 import CardMedia from "@material-ui/core/CardMedia";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
-import { useTheme, makeStyles } from "@material-ui/core/styles";
+import { useTheme, makeStyles } from '@material-ui/core/styles';
+import { description } from "../data/description";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   description: {
     marginTop: 50,
-    fontSize: "18px",
+    fontSize: '18px',
     color: "#674932"
   },
   descriptionXS: {
@@ -17,23 +19,23 @@ const useStyles = makeStyles(theme => ({
     color: "#c5a287",
     cursor: "pointer"
   },
-  link: {
-    textDecoration: "none"
+  link:{
+    textDecoration: 'none'
   },
   mainBody: {
     padding: 20,
-    width: "900px",
-    textAlign: "justify",
-    fontFamily: "Optima",
-    backgroundColor: "white",
-    boxShadow: "1px 1px 25px 10px #674932",
-    borderRadius: "5px"
+    width: '900px',
+    textAlign: 'justify',
+    fontFamily: 'Optima',
+    backgroundColor: 'white',
+    boxShadow: '1px 1px 25px 10px #674932',
+    borderRadius: '5px'
   },
   hr: {
-    height: "4px",
-    borderWidth: "0",
-    backgroundColor: "#c5a287",
-    borderRadius: "3px"
+    height: '4px',
+    borderWidth: '0',
+    backgroundColor: '#c5a287',
+    borderRadius: '3px'
   }
 }));
 
@@ -46,41 +48,34 @@ const LandingPage = props => {
   const isXS = useMediaQuery(theme.breakpoints.down("xs"));
 
   return (
-    <Grid
-      style={{ marginTop: 120 }}
-      justify="center"
-      alignItems="center"
-      container
-    >
-      <Grid className={classes.mainBody}>
+    <Grid style={{ marginTop: 120 }} justify="center" alignItems="center" container>
+      <Grid className={classes.mainBody} >
         <hr className={classes.hr} />
         <Grid spacing={3} style={{ padding: 20 }} container>
-          <Grid
-            sm={6}
-            xs={12}
-            className={isXS ? classes.descriptionXS : classes.description}
-            item
-          >
-            The application allows user to view a dashboard displaying 50
-            profiles with profile image, name and age with pagination (only 10
-            profiles per page).
-            <p />
-            User can {""}
-            <span
-              onClick={e => {
+          <Grid sm={6} xs={12} className={isXS ? classes.descriptionXS : classes.description} item>
+            {description.description1}
+            <p/>{description.description2}
+            <span>{isAuthenticated ?
+              <>{description.login}</>
+              :
+              <span onClick={e => {
                 e.preventDefault();
-                props.history.push("/login");
-              }}
-              className={classes.linkText}
-            >
-              login
-            </span>
-            {""} to the application with the credentials provided. Once logged out
-            user will no longer have access to the dashboard page.
+                props.history.push('/login');
+              }} className={classes.linkText}>
+                {description.login}</span>}</span>
+            {description.description3}
+            <a href="https://github.com/tinaPangging/dashboard/blob/main/src/data/credentials.js" className={classes.link}>
+              <span className={classes.linkText}>{description.credentials}</span>
+            </a>
+            {description.description4}
           </Grid>
           <Grid sm={6} xs={12} item>
             <CardActionArea>
-              <CardMedia image="image.jpg" title="background" component="img" />
+              <CardMedia
+                image='image.jpg'
+                title="background"
+                component="img"
+              />
             </CardActionArea>
           </Grid>
         </Grid>
